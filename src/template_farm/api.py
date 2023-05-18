c559948c798e31a5f35c5d2376a2f9a36154f21d
oedokumaci <oral.ersoy.dokumaci@gmail.com>
2023-05-18 14:18:23 +0300

    feat: add code reloading good to have when developing

--- src/template_farm/api.py
+++ src/template_farm/api.py
@@ -6,8 +6,9 @@ from fastapi import FastAPI
 from template_farm.path import LOGS_DIR
 from template_farm.utils import find_available_port
 
-app = FastAPI()
 log_file: pathlib.Path = LOGS_DIR / "api.log"
+
+app = FastAPI()
 
 
 def run_server() -> None:
@@ -15,7 +16,13 @@ def run_server() -> None:
     log_file.unlink(missing_ok=True)
     log_config: pathlib.Path = LOGS_DIR / "logging.conf"
     port = find_available_port(8000, 9999)
-    uvicorn.run(app, host="0.0.0.0", port=port, log_config=str(log_config))
+    uvicorn.run(
+        "template_farm.api:app",
+        host="0.0.0.0",
+        port=port,
+        log_config=str(log_config),
+        reload=True,  # Enable code reloading
+    )
 
 
 @app.get("/")
